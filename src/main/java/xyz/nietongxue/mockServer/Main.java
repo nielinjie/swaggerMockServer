f877c79b5f1ec34afc6d97216e4951e3e2fa64a0
nielinjie <nielinjie@gmail.com>
2016-09-10 23:19:55 +0800

    logs

--- src/main/java/xyz/nietongxue/mockServer/Main.java
+++ src/main/java/xyz/nietongxue/mockServer/Main.java
@@ -5,7 +5,9 @@ package xyz.nietongxue.mockServer;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-
+//        final StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+//        System.out.println(binder.getLoggerFactory());
+//        System.out.println(binder.getLoggerFactoryClassStr());
         MockServer mockServer=new MockServer(args[0]);
         mockServer.start();
 
